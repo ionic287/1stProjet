df3c3787bfa51a99e81409459bf9c7923b9abb6e
ionic287 <ionic28@yahoo.com>
2026-08-21 14:32:23 +0200

    Add print statement for pandas module

--- SimplonEx.py
+++ SimplonEx.py
@@ -3,3 +3,4 @@ import pandas as pd
 
 données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
+print pd
